37709ce65aaa2cff85e9773ad00b0395ff5d8445
notfoundium <t.r.yakubov@gmail.com>
2024-02-09 18:42:58 +0300

    Desc fix

--- src/main.py
+++ src/main.py
@@ -21,10 +21,10 @@ def get_courses(
     if exchanger is not None:
         if exchanger == ExchangerName.BINANCE:
             return session.query(Exchange).filter(Exchange.exchanger == ExchangerName.BINANCE).order_by(
-                Exchange.timestamp).first()
+                Exchange.timestamp.desc()).first()
         elif exchanger == ExchangerName.COINGECKO:
             return session.query(Exchange).filter(Exchange.exchanger == ExchangerName.COINGECKO).order_by(
-                Exchange.timestamp).first()
+                Exchange.timestamp.desc()).first()
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid exchanger name")
-    return session.query(Exchange).order_by(Exchange.timestamp).first()
+    return session.query(Exchange).order_by(Exchange.timestamp.desc()).first()
